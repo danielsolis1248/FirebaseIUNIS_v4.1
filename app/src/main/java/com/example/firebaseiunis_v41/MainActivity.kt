af0077b09d5a4b20c72d8b2c9a50a3133de8059a
danielsolis1248 <itic18.daniel.solis@iunis.edu.mx>
2022-10-26 11:19:13 -0500

    Huella digital, item logo y Linear layout

--- app/src/main/java/com/example/firebaseiunis_v41/MainActivity.kt
+++ app/src/main/java/com/example/firebaseiunis_v41/MainActivity.kt
@@ -1,32 +1,50 @@
 package com.example.firebaseiunis_v40
 
-import android.net.wifi.p2p.WifiP2pManager.ActionListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.firebaseiunis_v41.R
+import com.example.firebaseiunis_v41.databinding.ActivityMainBinding
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var authStateListener: FirebaseAuth.AuthStateListener
 
     private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
 
+        val response = IdpResponse.fromResultIntent(it.data)
+
             if (it.resultCode == RESULT_OK) {
                 val user = FirebaseAuth.getInstance().currentUser
+
                 if (user != null) {
                     Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+                } else {
+
+                    if (response == null) {
+                        Toast.makeText(this,"Adios...", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
                 }
             }
-        configuAuth()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configuAuth()
+    }
 
     private fun configuAuth() {
 
@@ -34,18 +52,20 @@ class MainActivity : AppCompatActivity() {
         authStateListener = FirebaseAuth.AuthStateListener { auth ->
             if (auth.currentUser != null) {
                 supportActionBar?.title = auth.currentUser?.displayName
-            } else {
-                val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+                binding.tvInit.visibility = View.VISIBLE
+                binding.llProgress.visibility = View.VISIBLE
 
+            } else {
+                val providers = arrayListOf(
+                    AuthUI.IdpConfig.EmailBuilder().build(),
+                    AuthUI.IdpConfig.GoogleBuilder().build())
                 resultLauncher.launch(
                     AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(providers)
                         .build()
                 )
-
             }
-
         }
     }
 
@@ -71,15 +91,16 @@ class MainActivity : AppCompatActivity() {
                     .addOnSuccessListener {
                         Toast.makeText(this, "Sesion Terminada", Toast.LENGTH_SHORT).show()
                     }
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            binding.tvInit.visibility = View.GONE
+                            binding.llProgress.visibility = View.GONE
+                        } else {
+                            Toast.makeText(this, "No se pudo terminar", Toast.LENGTH_SHORT).show()
+                        }
+                    }
             }
         }
         return super.onOptionsItemSelected(item)
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-
 }
-
